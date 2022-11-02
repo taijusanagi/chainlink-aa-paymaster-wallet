780e5bcceca0efa80027c19607bf76a0b84bcbfb
taijusanagi <taijusanagi@gmail.com>
2022-11-02 17:12:03 +0900

    chore: add deploy script

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,5 +1,25 @@
+/* eslint-disable camelcase */
+import { EntryPoint__factory } from "@account-abstraction/contracts";
+import fs from "fs";
+import { ethers } from "hardhat";
+import path from "path";
+
+import { DeterministicDeployer } from "../lib/infinitism/DeterministicDeployer";
+import { CapsuleWalletDeployer__factory } from "../typechain-types";
+
 async function main() {
-  // TODO: implement
+  const argument = ethers.utils.defaultAbiCoder.encode(["uint256", "uint256"], [1, 1]);
+  const entryPointCreationCode = ethers.utils.solidityPack(
+    ["bytes", "bytes"],
+    [EntryPoint__factory.bytecode, argument]
+  );
+  const entryPointAddress = await DeterministicDeployer.deploy(entryPointCreationCode);
+  const factoryAddress = await DeterministicDeployer.deploy(CapsuleWalletDeployer__factory.bytecode);
+  const result = {
+    entryPoint: entryPointAddress,
+    factory: factoryAddress,
+  };
+  fs.writeFileSync(path.join(__dirname, `../deployments.json`), JSON.stringify(result));
 }
 
 main().catch((error) => {
