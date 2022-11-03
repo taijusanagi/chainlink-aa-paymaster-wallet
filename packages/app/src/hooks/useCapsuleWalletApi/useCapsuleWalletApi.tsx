889f0723d44f1271b853475d6bcebaf624ed1a19
taijusanagi <taijusanagi@gmail.com>
2022-11-03 10:35:46 +0900

    feat: add account abstraction hook

---
+++ packages/app/src/hooks/useCapsuleWalletApi/useCapsuleWalletApi.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable camelcase */
+import { HttpRpcClient } from "@account-abstraction/sdk/dist/src/HttpRpcClient";
+import { ethers } from "ethers";
+import { useEffect, useMemo, useState } from "react";
+import { useSigner } from "wagmi";
+
+import rpc from "../../../../contracts/config/rpc.json";
+import deployments from "../../../../contracts/deployments.json";
+import { CapsuleWalletAPI } from "../../../../contracts/lib/CapsuleWalletAPI";
+
+export const useCapsuleWalletAPI = () => {
+  const { data: signer } = useSigner();
+
+  const [signerAddress, setSignerAddress] = useState("");
+  const [capsuleWalletAPI, setCapsuleWalletAPI] = useState<CapsuleWalletAPI>();
+  const [capsuleWalletAddress, setCapsuleWalletAddress] = useState("");
+  const [capsuleWalletBalance, setCapsuleWalletBalance] = useState("0.0");
+
+  const bundler = useMemo(() => {
+    return new HttpRpcClient("http://localhost:3001/rpc", deployments.entryPoint, 5);
+  }, []);
+
+  const provider = useMemo(() => {
+    return new ethers.providers.JsonRpcProvider(rpc.goerli);
+  }, []);
+
+  const signAndSendTxWithBundler = async (target: string, data: string, value: string) => {
+    if (!capsuleWalletAPI) {
+      return;
+    }
+    const op = await capsuleWalletAPI.createSignedUserOp({
+      target,
+      data,
+      value,
+    });
+    return await bundler.sendUserOpToBundler(op);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (!signer) {
+        setCapsuleWalletAPI(undefined);
+        setCapsuleWalletAddress("");
+        return;
+      }
+      const capsuleWalletAPI = new CapsuleWalletAPI({
+        provider,
+        entryPointAddress: deployments.entryPoint,
+        owner: signer,
+        factoryAddress: deployments.factory,
+      });
+
+      setCapsuleWalletAPI(capsuleWalletAPI);
+      signer.getAddress().then((signerAddress) => setSignerAddress(signerAddress));
+
+      const capsuleWalletAddress = await capsuleWalletAPI.getWalletAddress();
+      setCapsuleWalletAddress(capsuleWalletAddress);
+      provider.getBalance(capsuleWalletAddress).then((capsuleWalletBalanceBigNumber) => {
+        const capsuleWalletBalance = ethers.utils.formatEther(capsuleWalletBalanceBigNumber);
+        setCapsuleWalletBalance(capsuleWalletBalance);
+      });
+    })();
+  }, [provider, signer]);
+
+  return {
+    signerAddress,
+    capsuleWalletAPI,
+    capsuleWalletAddress,
+    capsuleWalletBalance,
+    signAndSendTxWithBundler,
+  };
+};
