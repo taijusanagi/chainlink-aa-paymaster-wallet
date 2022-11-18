460433e7a3cf8edd4cef9b4fb787074bff8a7b16
taijusanagi <taijusanagi@gmail.com>
2022-11-18 15:58:49 +0900

    feat: add sign in with ethereum

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -3,7 +3,9 @@ import "@fontsource/inter/variable.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
 import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { WagmiConfig } from "wagmi";
 
 import { myChakraUITheme, myRainbowKitTheme } from "@/lib/theme";
@@ -13,9 +15,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider resetCSS theme={myChakraUITheme}>
       <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} showRecentTransactions={true} theme={myRainbowKitTheme}>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
+        <SessionProvider refetchInterval={0} session={pageProps.session}>
+          <RainbowKitSiweNextAuthProvider>
+            <RainbowKitProvider chains={chains} showRecentTransactions={true} theme={myRainbowKitTheme}>
+              <Component {...pageProps} />
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </SessionProvider>
       </WagmiConfig>
     </ChakraProvider>
   );
